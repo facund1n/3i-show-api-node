25ec42946b1f672641af34722039a3e653b27e90
facund1n <facus16s@gmail.com>
2022-11-07 13:16:28 -0300

    exprees default config

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hola Wosadasd");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app listening on port: ${port}`);
 });
