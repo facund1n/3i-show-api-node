9ae61620d6f89c691939f31aa084b14cdd1bb996
facund1n <facus16s@gmail.com>
2022-11-24 00:22:56 -0300

    galerias update

--- routes/galerias.js
+++ routes/galerias.js
@@ -4,7 +4,7 @@ const Galeria = require("../models/galeria");
 router
   // Obtener todos las fotos/galería:
   .get("/all", async (req, res) => {
-    console.log("GET /galeria/all");
+    console.log("GET /galerias/all");
     try {
       const allGaleria = await Galeria.find();
       res.status(200).send(allGaleria);
@@ -17,7 +17,7 @@ router
   // Obtener una sola foto/galería:
   .get("/:id", async (req, res) => {
     const { id } = req.params;
-    console.log("GET /galeria/id" + id);
+    console.log("GET /galerias/id" + id);
     try {
       const galeria = await Galeria.findOne({ _id: id });
       res.status(200).json(galeria);
@@ -26,13 +26,13 @@ router
     }
   }) // Postear una nueva foto/galería:
   .post("/new", async (req, res) => {
-    console.log("POST /galeria/new");
+    console.log("POST /galerias/new");
     const { body } = req;
     try {
       const newGaleria = new Galeria(body);
       await newGaleria.save();
       res.status(200).json(newGaleria);
-      console.log("ADD/POST /galería/id" + newGaleria._id);
+      console.log("ADD/POST /galerias/id" + newGaleria._id);
     } catch (error) {
       console.log(error);
       res.status(400).json({ error: true, message: error });
@@ -42,13 +42,13 @@ router
   .patch("/update/:id", async (req, res) => {
     const { id } = req.params;
     const { body } = req;
-    console.log("PUT/UPDATE galeria/id" + id);
+    console.log("PUT/UPDATE galerias/id" + id);
     try {
       const modGaleria = await Galeria.findByIdAndUpdate(id, body, {
         useFindAndModify: false,
       });
       res.status(200).json(modGaleria);
-      console.log("UPDATE galeria/id " + modGaleria._id);
+      console.log("UPDATE galerias/id " + modGaleria._id);
     } catch (error) {
       console.log(error);
       res.status(404).json({
