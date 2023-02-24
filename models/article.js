e9096687d6aac459d31622d63d908725321464c2
facund1n <facus16s@gmail.com>
2023-02-24 20:41:18 -0300

    schema syntax fix

--- models/article.js
+++ models/article.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-  title: { String, required: true, min: 2, max: 85 },
-  subtitle: { String, required: true, min: 2, max: 85 },
-  body: { String, required: true, min: 10, max: 500 },
-  image: { String, required: true },
-  alt: { String, required: true },
+  title: { type: String, required: true, min: 2, max: 85 },
+  subtitle: { type: String, required: true, min: 2, max: 85 },
+  body: { type: String, required: true, min: 10, max: 500 },
+  image: { type: String, required: true },
+  alt: { type: String, required: true },
   category: { String, required: true },
   likes: Array,
   ticketsUrl: String,
